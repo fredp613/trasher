db981cb52016cbaa3b1eb69855d9979bc46c2b20
fredp613 <fredp613@gmail.com>
2014-10-18 14:12:43 -0400

    Initial Commit

---
+++ TrasherTests/TrasherTests.swift
@@ -0,0 +1,36 @@
+//
+//  TrasherTests.swift
+//  TrasherTests
+//
+//  Created by Fred Pearson on 2014-10-18.
+//  Copyright (c) 2014 Frederick Pearson. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class TrasherTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
